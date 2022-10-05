1c1e8e5f63f5218025519ac5523f162808a2a89b
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2022-10-05 14:42:32 +0900

    カードの移動

---
+++ Assets/Testcard Folder/Scriput/TestCardMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TestCardMove : MonoBehaviour, IDragHandler,IBeginDragHandler,IEndDragHandler
+{
+    public Transform defaultParent;
+    /// <summary>
+    /// カードを持った時
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        defaultParent = transform.parent;
+        transform.SetParent(defaultParent.parent, false);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    /// <summary>
+    /// カードを持ってる最中
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    /// <summary>
+    /// カードを置いた時
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(defaultParent, false);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
+
---
+++ Assets/Testcard Folder/Scriput/TsetDropPlace.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TsetDropPlace : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        TestCardMove card = eventData.pointerDrag.GetComponent<TestCardMove>();
+        if (card != null)
+        {
+            card.defaultParent = this.transform;
+        }
+    }
+}
